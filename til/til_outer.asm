4975551985fe846bb79aeb7dcf63571a76979c3e
kloyd <kelly.loyd@icloud.com>
2024-01-05 15:49:14 -0600

    Shell of Outer Interpreter

---
+++ til/til_outer.asm
@@ -0,0 +1,33 @@
+; TIL
+; Outer Interpreter
+
+; TIL
+
+; START/RESTART
+
+; TYPE
+
+; INLINE
+
+; ASPACE
+
+; TOKEN
+
+; ?SEARCH
+
+; ABSENT?
+; - NO -> ?EXECUTE -> ASPACE
+; - YES -> NUMBER
+
+; ?EXECUTE
+
+; ?NUMBER
+
+; INVALID NUMBER?
+; NO -> ASPACE
+; YES -> QUESTION
+
+; QUESTION
+
+; GOTO TYPE
+
